f79eb89a4a89901dc55db59c51e1c64c521cfc2d
sunilchau <sunil.chaudhari7@gmail.com>
2022-12-31 18:57:25 +0530

    Login page added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Happy New Year 2023"
+   #return "<h1 style = 'color:green'> Happy New Year 2023</h1>"
+    return render_template('login.html')
 
 app.run(debug = True)
