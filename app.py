af3fe474c71b782f4afa0752fe7479f97eac6f07
sunilchau <sunil.chaudhari7@gmail.com>
2023-01-01 11:24:01 +0530

    NER page added

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 from db import Database
+
 app = Flask(__name__)
 dbo = Database()
+
+
 @app.route("/")
 def index():
     # return "<h1 style = 'color:green'> Happy New Year 2023</h1>"
@@ -16,16 +19,40 @@ def registration():
 @app.route("/perform_registration", methods=['post'])
 def performregistration():
     name = request.form.get("user_ka_name")
-    #print(name)
+    # print(name)
     email = request.form.get("user_ka_email")
-    #print(email)
+    # print(email)
     password = request.form.get("user_ka_password")
-    #print(email)
+    # print(email)
     response = dbo.insert(name, email, password)
     if response:
-        return render_template('login.html',message = "Registration Successful. Kindly login to proceed")
+        return render_template('login.html', message="Registration Successful. Kindly login to proceed")
     else:
-        return render_template('register.html',message = "Email Already exists")
+        return render_template('register.html', message="Email Already exists")
 
+
+@app.route("/perform_login", methods=['post'])
+def perform_login():
+    email = request.form.get("user_ka_email")
+    password = request.form.get("user_ka_password")
+
+    response = dbo.search(email, password)
+    if response:
+        return redirect("/profile")
+    else:
+        return render_template('login.html', message="incorrect email/password")
+
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+@app.route("/ner")
+def ner():
+    return render_template('ner.html')
+
+@app.route("/perform_ner", methods =['post'])
+def perform_ner():
+    return "Yeha NER Hoga"
 
 app.run(debug=True)
